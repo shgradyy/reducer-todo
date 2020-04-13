c59e2a012431b553fef9bfd14431b227ea2836aa
Shantay Grady <shantaygrady@gmail.com>
2020-04-13 03:18:56 -0400

    Build simple reducer int state

---
+++ reducertodo/src/reducers/reducerComp.js
@@ -0,0 +1,29 @@
+export default function() {
+    return [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 1
+          },
+          {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 2
+          },
+          {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3
+          },
+          {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 4
+          },
+          {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 5
+          }
+        ]
+}
